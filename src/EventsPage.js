dda2b4341e99c29189419bcae05feca0d3f1b6ba
TrungCorp <trungle9995@gmail.com>
2024-08-18 18:54:36 -0400

    created EventsPage component, edited css file

---
+++ src/EventsPage.js
@@ -0,0 +1,11 @@
+import NavBar from "./NavBar"
+function EventsPage(){
+    return(
+        <div>
+            <NavBar></NavBar>
+            <h>EVENTS PAGE COMPONENT</h>
+        </div>
+    )
+}
+
+export default EventsPage
